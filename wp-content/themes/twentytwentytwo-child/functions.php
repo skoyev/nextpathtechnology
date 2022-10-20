5f558921bb1460d10295a6be79ca6edce07c4c78
skoyev <skoyev@gmail.com>
2022-10-19 22:41:39 -0400

    Dev

--- wp-content/themes/twentytwentytwo-child/functions.php
+++ wp-content/themes/twentytwentytwo-child/functions.php
@@ -1,13 +1,14 @@
 <?php
 
-/*add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
 function my_theme_enqueue_styles() {
+    /*
     wp_enqueue_style( 'child-style', get_stylesheet_uri(),
                       array( 'twenty-twenty-two-style' ), 
                       wp_get_theme()->;get('Version') );
-    );
-}*/
+    );*/
+}
 
 /*
 function twentytwentyone_styles() {
